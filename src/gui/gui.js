3a6bd630cf27dc56b8b6b1f7d847ce565d77ef78
AlexanderDzhoganov <alexander.dzhoganov@gmail.com>
2014-06-24 13:08:06 +0300

    refactored gui input code to guiinput.js

--- src/gui/gui.js
+++ src/gui/gui.js
@@ -18,74 +18,7 @@ var Gui = function ()
 
 	this._previousTime = 0.0;
 
-	this._mousePosition = vec2.create();
-	this._cursor = new Cursor("cursor");
-	this._mouseDown = false;
-	this._mouseUp = false;
-
-	this._keyBuffer = "";
-	this._caretIndex = 0;
-	this._caretLineIndex = 0;
-
-	document.body.onclick = document.body.requestPointerLock || document.body.mozRequestPointerLock || document.body.webkitRequestPointerLock;
-
-	document.onmousedown = function ()
-	{
-		this._mouseDown = true;
-		this._mouseUp = false;
-	}.bind(this);
-
-	document.onmouseup = function ()
-	{
-		this._mouseDown = false;
-		this._mouseUp = true;
-	}.bind(this);
-
-	window.addEventListener("keydown", function(e)
-	{
-		if (e.keyCode == 8)
-		{
-			e.preventDefault();
-		}
-		else if (e.keyCode == 13) // enter
-		{
-			e.preventDefault();
-		}
-	});
-
-	window.addEventListener("keyup", function(e)
-	{		
-		if (e.keyCode == 8) // backspace
-		{
-			this._injectBackspace();
-			e.preventDefault();
-		}
-		else if (e.keyCode == 13) // enter
-		{
-			this._injectenter();
-			e.preventDefault();
-		}
-		else if (e.keyCode == 37) // left
-		{
-			this._injectLeft();
-		}
-		else if (e.keyCode == 39) // right
-		{
-			this._injectRight();
-		}
-		else if (e.keyCode == 38) // up
-		{
-			this._injectUp();
-		}
-		else if (e.keyCode == 40) // down
-		{
-			this._injectDown();
-		}
-		else
-		{
-			this._injectChar(e);
-		}
-	}.bind(this));
+	this._input = new GuiInput();
 };
 
 Gui.extend(
@@ -137,7 +70,7 @@ Gui.prototype.extend(
 		GL.texImage2D(GL.TEXTURE_2D, 0, GL.RGBA, GL.RGBA, GL.UNSIGNED_BYTE, this._canvas);
 		Renderer.drawFullscreenQuad();
 
-		this._cursor.renderSelf();
+		this._input._cursor.renderSelf();
 	},
 
 	getModelMatrix: function ()
@@ -163,7 +96,7 @@ Gui.prototype.extend(
 		vec2.subtract(relativeMousePosition, this.cursorPosition, rect.position);
 
 		var hovered = PointRectTest(this.cursorPosition, rect.position, rect.size) && this._activeWindow == wnd;
-		var clicked = hovered && this._mouseDown;
+		var clicked = hovered && this._input._mouseDown;
 
 		if(wnd._resizing || wnd._dragging)
 		{
@@ -175,7 +108,7 @@ Gui.prototype.extend(
 		{
 			this._activeControlPosition = rect.position;
 			this._activeControlSize = rect.size;
-			this._mouseDown = false;
+			this._input._mouseDown = false;
 		}
 
 		var state = "normal";
@@ -205,8 +138,8 @@ Gui.prototype.extend(
 			state: state,
 			relativeMousePosition: relativeMousePosition,
 			mousePosition: this.cursorPosition,
-			caretIndex: this._caretIndex,
-			caretLineIndex: this._caretLineIndex,
+			caretIndex: this._input._caretIndex,
+			caretLineIndex: this._input._caretLineIndex,
 
 		};
 	},
@@ -336,13 +269,13 @@ Gui.prototype.extend(
 
 				if (control.clicked)
 				{
-					this._keyBuffer = input;
-					this._caretIndex = wnd._calculateInputBoxCaretIndex(this._context, input, control);
+					this._input._keyBuffer = input;
+					this._input._caretIndex = wnd._calculateInputBoxCaretIndex(this._context, input, control);
 				}
 
-				if(this._caretIndex > input.length + 1)
+				if(this._input._caretIndex > input.length + 1)
 				{
-					this._caretIndex = input.length + 1;
+					this._input._caretIndex = input.length + 1;
 				}
 
 				wnd._drawInputBox(this._context, input, control);
@@ -351,7 +284,7 @@ Gui.prototype.extend(
 
 				if (control.active)
 				{
-					var newInput = this._keyBuffer;
+					var newInput = this._input._keyBuffer;
 
 					if(newInput.length > maxLength)
 					{
@@ -370,10 +303,10 @@ Gui.prototype.extend(
 
 				if (control.clicked)
 				{
-					this._keyBuffer = input;
+					this._input._keyBuffer = input;
 					var carets = wnd._calculateTextBoxCaretIndex(this._context, input, rows, cols, control);
-					this._caretIndex = carets[0];
-					this._caretLineIndex = carets[1];
+					this._input._caretIndex = carets[0];
+					this._input._caretLineIndex = carets[1];
 				}
 
 				wnd._drawTextBox(this._context, input, rows, cols, control);
@@ -381,7 +314,7 @@ Gui.prototype.extend(
 
 				if(control.active)
 				{
-					var newInput = this._keyBuffer;
+					var newInput = this._input._keyBuffer;
 					return newInput;
 				}
 
@@ -469,7 +402,7 @@ Gui.prototype.extend(
 			vec2.fromValues(wnd.position[0] + wnd.size[0], wnd.position[1] + wnd.size[1])
 		);
 
-		if (closeButtonHovered && this._mouseDown)
+		if (closeButtonHovered && this._input._mouseDown)
 		{
 			if (wnd.onClose)
 			{
@@ -477,17 +410,17 @@ Gui.prototype.extend(
 			}
 		}
 
-		if (this._activeWindow == wnd && this._mouseDown)
+		if (this._activeWindow == wnd && this._input._mouseDown)
 		{
 			this.bringToFront(wnd);
 		}
 
-		if (this._activeWindow == wnd && headerHovered && this._mouseDown && !wnd._dragging)
+		if (this._activeWindow == wnd && headerHovered && this._input._mouseDown && !wnd._dragging)
 		{
 			wnd._dragging = true;
 			vec2.subtract(wnd._dragAnchor, wnd.position, this.cursorPosition);
 		}
-		else if (this._activeWindow == wnd && wnd._dragging && this._mouseDown)
+		else if (this._activeWindow == wnd && wnd._dragging && this._input._mouseDown)
 		{
 			vec2.add(wnd.position, this.cursorPosition, wnd._dragAnchor);
 			wnd.position[0] = Clamp(wnd.position[0], 0.0, Renderer.screenWidth - wnd.size[0]);
@@ -498,14 +431,14 @@ Gui.prototype.extend(
 			wnd._dragging = false;
 		}
 
-		if(this._activeWindow == wnd && resizeButtonHovered && this._mouseDown && !wnd._resizing)
+		if(this._activeWindow == wnd && resizeButtonHovered && this._input._mouseDown && !wnd._resizing)
 		{
 			wnd._resizing = true;
 			var corner = vec2.create();
 			vec2.add(corner, wnd.position, wnd.size);
 			vec2.subtract(wnd._resizeAnchor, corner, this.cursorPosition);
 		}
-		else if(this._activeWindow == wnd && wnd._resizing && this._mouseDown)
+		else if(this._activeWindow == wnd && wnd._resizing && this._input._mouseDown)
 		{
 			var corner = vec2.create();
 			vec2.add(corner, wnd._resizeAnchor, this.cursorPosition);
@@ -542,7 +475,7 @@ Gui.prototype.extend(
 	{
 		this._context.clearRect(0, 0, Renderer.screenWidth, Renderer.screenHeight);
 
-		this.cursorPosition = vec2.fromValues(this._cursor.position[0], Renderer.screenHeight - this._cursor.position[1] - this._cursor.size[1] * 0.5);
+		this.cursorPosition = vec2.fromValues(this._input._cursor.position[0], Renderer.screenHeight - this._input._cursor.position[1] - this._input._cursor.size[1] * 0.5);
 
 		var time = (new Date).getTime();
 		var deltaTime = (time - this._previousTime) / 1000.0;
@@ -615,165 +548,6 @@ Gui.prototype.extend(
 		var vertexSource = Shader.GetSourceFromHTMLElement("gui-vertex-shader");
 		var fragmentSource = Shader.GetSourceFromHTMLElement("gui-fragment-shader");
 		this._program = Shader.CreateProgram(vertexSource, fragmentSource);
-	},
-
-	_injectBackspace: function ()
-	{
-		if(this._keyBuffer.indexOf('\n') != -1)
-		{
-			var lines = this._keyBuffer.split('\n');
-			var line = lines[this._caretLineIndex];
-			line = line.slice(0, this._caretIndex - 1) + line.slice(this._caretIndex, line.length);
-			lines[this._caretLineIndex] = line;
-
-			this._keyBuffer = "";
-			for(var i = 0; i < lines.length; i++)
-			{
-				this._keyBuffer += lines[i] + '\n';
-			}
-
-			if(this._keyBuffer.length > 0)
-			{
-				this._caretIndex--;
-			}
-		}
-		else
-		{
-			this._keyBuffer = this._keyBuffer.slice(0, this._caretIndex - 1) + this._keyBuffer.slice(this._caretIndex, this._keyBuffer.length);
-
-			if(this._keyBuffer.length > 0)
-			{
-				this._caretIndex--;
-			}
-		}
-	},
-
-	_injectEnter: function ()
-	{
-		if(this._keyBuffer.indexOf('\n') != -1)
-		{
-			var lines = this._keyBuffer.split('\n');
-			var line = lines[this._caretLineIndex];
-			line = line.slice(0, this._caretIndex) + '\n' + line.slice(this._caretIndex, line.length);
-			lines[this._caretLineIndex] = line;
-			this._caretLineIndex++;
-			this._caretIndex = 0;
-
-			this._keyBuffer = "";
-			for(var i = 0; i < lines.length; i++)
-			{
-				this._keyBuffer += lines[i] + '\n';
-			}
-		}
-		else
-		{
-			this._activeControlPosition = vec2.fromValues(0.0, 0.0);
-			this._activeControlSize = vec2.fromValues(0.0, 0.0);
-		}
-	},
-
-	_injectUp: function ()
-	{
-		if(this._keyBuffer.indexOf('\n') == -1)
-		{
-			return;
-		}
-
-		this._caretLineIndex--;
-		if(this._caretLineIndex < 0)
-		{
-			this._caretLineIndex = 0;
-		}
-	},
-
-	_injectDown: function ()
-	{
-		if(this._keyBuffer.indexOf('\n') == -1)
-		{
-			return;
-		}
-
-		this._caretLineIndex++;
-
-		var numNewLines = this._keyBuffer.match(/\n/g).length;
-		if(this._caretLineIndex > numNewLines)
-		{
-			this._caretLineIndex = numNewLines;
-		}
-	},
-
-	_injectLeft: function ()
-	{
-		if(this._keyBuffer.indexOf('\n') != -1)
-		{
-			this._caretIndex--;
-			if(this._caretIndex < 0)
-			{
-				var lines = this._keyBuffer.split('\n');
-				if(this._caretLineIndex > 0)
-				{
-					this._caretIndex = lines[this._caretLineIndex].length + 1;
-					this._caretLineIndex--;
-				}
-			}
-		}
-		else
-		{
-			this._caretIndex--;
-
-			if(this._caretIndex < 0)
-			{
-				this._caretIndex = 0;
-			}
-		}			
-	},
-
-	_injectRight: function ()
-	{
-		if(this._keyBuffer.indexOf('\n') != -1)
-		{
-			this._caretIndex++;
-			var lines = this._keyBuffer.split('\n');
-
-			if(this._caretIndex > lines[this._caretLineIndex].length)
-			{
-				this._caretIndex = lines[this._caretLineIndex].length;
-
-				if(this._caretLineIndex < lines.length)
-				{
-					this._caretLineIndex++;
-					this._caretIndex = 0;
-				}
-			}
-		}
-		else
-		{
-			this._caretIndex++;
-		}
-	},
-
-	_injectChar: function (e)
-	{
-		if(this._keyBuffer.indexOf('\n') != -1)
-		{
-			var lines = this._keyBuffer.split('\n');
-			var line = lines[this._caretLineIndex];
-			line = line.slice(0, this._caretIndex) + String.fromCharCode(e.which).toLowerCase() + line.slice(this._caretIndex, line.length);
-			lines[this._caretLineIndex] = line;
-
-			this._keyBuffer = "";
-			for(var i = 0; i < lines.length; i++)
-			{
-				this._keyBuffer += lines[i] + '\n';
-			}
-
-			this._caretIndex++;
-		}
-		else
-		{
-			this._keyBuffer = this._keyBuffer.slice(0, this._caretIndex) + String.fromCharCode(e.which).toLowerCase() + this._keyBuffer.slice(this._caretIndex, this._keyBuffer.length);
-			this._caretIndex++;
-		}
 	},
 
 });
---
+++ src/gui/guiinput.js
@@ -0,0 +1,240 @@
+var GuiInput = function ()
+{
+	this._mousePosition = vec2.create();
+	this._cursor = new Cursor("cursor");
+	this._mouseDown = false;
+	this._mouseUp = false;
+
+	this._keyBuffer = "";
+	this._caretIndex = 0;
+	this._caretLineIndex = 0;
+
+	document.body.onclick = document.body.requestPointerLock || document.body.mozRequestPointerLock || document.body.webkitRequestPointerLock;
+
+	document.onmousedown = function ()
+	{
+		this._mouseDown = true;
+		this._mouseUp = false;
+	}.bind(this);
+
+	document.onmouseup = function ()
+	{
+		this._mouseDown = false;
+		this._mouseUp = true;
+	}.bind(this);
+
+	window.addEventListener("keydown", function(e)
+	{
+		if (e.keyCode == 8)
+		{
+			e.preventDefault();
+		}
+		else if (e.keyCode == 13) // enter
+		{
+			e.preventDefault();
+		}
+	});
+
+	window.addEventListener("keyup", function(e)
+	{		
+		if (e.keyCode == 8) // backspace
+		{
+			this._injectBackspace();
+			e.preventDefault();
+		}
+		else if (e.keyCode == 13) // enter
+		{
+			this._injectenter();
+			e.preventDefault();
+		}
+		else if (e.keyCode == 37) // left
+		{
+			this._injectLeft();
+		}
+		else if (e.keyCode == 39) // right
+		{
+			this._injectRight();
+		}
+		else if (e.keyCode == 38) // up
+		{
+			this._injectUp();
+		}
+		else if (e.keyCode == 40) // down
+		{
+			this._injectDown();
+		}
+		else
+		{
+			this._injectChar(e);
+		}
+	}.bind(this));
+};
+
+GuiInput.extend(
+{
+	
+});
+
+GuiInput.prototype.extend(
+{
+	
+	_injectBackspace: function ()
+	{
+		if(this._keyBuffer.indexOf('\n') != -1)
+		{
+			var lines = this._keyBuffer.split('\n');
+			var line = lines[this._caretLineIndex];
+			line = line.slice(0, this._caretIndex - 1) + line.slice(this._caretIndex, line.length);
+			lines[this._caretLineIndex] = line;
+
+			this._keyBuffer = "";
+			for(var i = 0; i < lines.length; i++)
+			{
+				this._keyBuffer += lines[i] + '\n';
+			}
+
+			if(this._keyBuffer.length > 0)
+			{
+				this._caretIndex--;
+			}
+		}
+		else
+		{
+			this._keyBuffer = this._keyBuffer.slice(0, this._caretIndex - 1) + this._keyBuffer.slice(this._caretIndex, this._keyBuffer.length);
+
+			if(this._keyBuffer.length > 0)
+			{
+				this._caretIndex--;
+			}
+		}
+	},
+
+	_injectEnter: function ()
+	{
+		if(this._keyBuffer.indexOf('\n') != -1)
+		{
+			var lines = this._keyBuffer.split('\n');
+			var line = lines[this._caretLineIndex];
+			line = line.slice(0, this._caretIndex) + '\n' + line.slice(this._caretIndex, line.length);
+			lines[this._caretLineIndex] = line;
+			this._caretLineIndex++;
+			this._caretIndex = 0;
+
+			this._keyBuffer = "";
+			for(var i = 0; i < lines.length; i++)
+			{
+				this._keyBuffer += lines[i] + '\n';
+			}
+		}
+		else
+		{
+			this._activeControlPosition = vec2.fromValues(0.0, 0.0);
+			this._activeControlSize = vec2.fromValues(0.0, 0.0);
+		}
+	},
+
+	_injectUp: function ()
+	{
+		if(this._keyBuffer.indexOf('\n') == -1)
+		{
+			return;
+		}
+
+		this._caretLineIndex--;
+		if(this._caretLineIndex < 0)
+		{
+			this._caretLineIndex = 0;
+		}
+	},
+
+	_injectDown: function ()
+	{
+		if(this._keyBuffer.indexOf('\n') == -1)
+		{
+			return;
+		}
+
+		this._caretLineIndex++;
+
+		var numNewLines = this._keyBuffer.match(/\n/g).length;
+		if(this._caretLineIndex > numNewLines)
+		{
+			this._caretLineIndex = numNewLines;
+		}
+	},
+
+	_injectLeft: function ()
+	{
+		if(this._keyBuffer.indexOf('\n') != -1)
+		{
+			this._caretIndex--;
+			if(this._caretIndex < 0)
+			{
+				var lines = this._keyBuffer.split('\n');
+				if(this._caretLineIndex > 0)
+				{
+					this._caretIndex = lines[this._caretLineIndex].length + 1;
+					this._caretLineIndex--;
+				}
+			}
+		}
+		else
+		{
+			this._caretIndex--;
+
+			if(this._caretIndex < 0)
+			{
+				this._caretIndex = 0;
+			}
+		}			
+	},
+
+	_injectRight: function ()
+	{
+		if(this._keyBuffer.indexOf('\n') != -1)
+		{
+			this._caretIndex++;
+			var lines = this._keyBuffer.split('\n');
+
+			if(this._caretIndex > lines[this._caretLineIndex].length)
+			{
+				this._caretIndex = lines[this._caretLineIndex].length;
+
+				if(this._caretLineIndex < lines.length)
+				{
+					this._caretLineIndex++;
+					this._caretIndex = 0;
+				}
+			}
+		}
+		else
+		{
+			this._caretIndex++;
+		}
+	},
+
+	_injectChar: function (e)
+	{
+		if(this._keyBuffer.indexOf('\n') != -1)
+		{
+			var lines = this._keyBuffer.split('\n');
+			var line = lines[this._caretLineIndex];
+			line = line.slice(0, this._caretIndex) + String.fromCharCode(e.which).toLowerCase() + line.slice(this._caretIndex, line.length);
+			lines[this._caretLineIndex] = line;
+
+			this._keyBuffer = "";
+			for(var i = 0; i < lines.length; i++)
+			{
+				this._keyBuffer += lines[i] + '\n';
+			}
+
+			this._caretIndex++;
+		}
+		else
+		{
+			this._keyBuffer = this._keyBuffer.slice(0, this._caretIndex) + String.fromCharCode(e.which).toLowerCase() + this._keyBuffer.slice(this._caretIndex, this._keyBuffer.length);
+			this._caretIndex++;
+		}
+	},
+
+});
