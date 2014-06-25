3d0f0b9fc301c3c9969901a55400d384202c776e
AlexanderDzhoganov <alexander.dzhoganov@gmail.com>
2014-06-25 18:03:53 +0300

    refactored meshrenderer

--- src/components/meshrenderer.js
+++ src/components/meshrenderer.js
@@ -30,6 +30,21 @@ MeshRenderer.prototype.extend(
 			return;
 		}
 
+		this._setupMaterial();
+
+		for(var i = 0; i < meshProvider.submeshes.length; i++)
+		{
+			this._drawSubmesh(meshProvider.submeshes[i]);
+		}
+
+		if(this.drawBounds)
+		{
+			this._drawBounds();
+		}
+	},
+
+	_setupMaterial: function ()
+	{
 		var material = this.material;
 		var program = this.material.program;
 
@@ -54,38 +69,36 @@ MeshRenderer.prototype.extend(
 			GL.bindTexture(GL.TEXTURE_2D, material.textures[name]._texture);
 			index++;
 		}
+	},
 
-		for(var i = 0; i < meshProvider.submeshes.length; i++)
+	_drawSubmesh: function (subMesh)
+	{
+		GL.bindBuffer(GL.ARRAY_BUFFER, subMesh.vertexBuffer);
+		GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, subMesh.indexBuffer);
+
+		if(subMesh.primitiveType == 'indexedTriangleStrip')
 		{
-			var subMesh = meshProvider.submeshes[i];
+			Renderer.drawIndexedTriangleStrip(subMesh.indices.length, subMesh.vertexFormat);
+		}
+		else if(subMesh.primitiveType == 'indexedTriangles')
+		{
+			Renderer.drawIndexedTriangles(subMesh.indices.length, subMesh.vertexFormat);	
+		}
+	},
 
-			GL.bindBuffer(GL.ARRAY_BUFFER, subMesh.vertexBuffer);
-			GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, subMesh.indexBuffer);
-
-			if(subMesh.primitiveType == 'indexedTriangleStrip')
-			{
-				Renderer.drawIndexedTriangleStrip(subMesh.indices.length, subMesh.vertexFormat);
-			}
-			else if(subMesh.primitiveType == 'indexedTriangles')
-			{
-				Renderer.drawIndexedTriangles(subMesh.indices.length, subMesh.vertexFormat);	
-			}
+	_drawBounds: function ()
+	{
+		var boundsProvider = this.gameObject.getComponent("meshBoundsProvider");
+		if(!boundsProvider)
+		{
+			console.log("MeshRenderer: drawBounds set but no meshBoundsProvider on the gameobject");
+			return;
 		}
 
-		if(this.drawBounds)
-		{
-			var boundsProvider = this.gameObject.getComponent("meshBoundsProvider");
-			if(!boundsProvider)
-			{
-				console.log("MeshRenderer: drawBounds set but no meshBoundsProvider on the gameobject");
-				return;
-			}
+		GL.bindBuffer(GL.ARRAY_BUFFER, boundsProvider.vertexBuffer);
+		GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, boundsProvider.indexBuffer);
 
-			GL.bindBuffer(GL.ARRAY_BUFFER, boundsProvider.vertexBuffer);
-			GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, boundsProvider.indexBuffer);
-
-			Renderer.drawIndexedLines(boundsProvider.indices.length);
-		}
+		Renderer.drawIndexedLines(boundsProvider.indices.length);
 	},
 
 });
