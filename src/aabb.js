7a225c74175647490c8315dddeafb1794c602ff2
AlexanderDzhoganov <alexander.dzhoganov@gmail.com>
2014-06-26 12:40:04 +0300

    AABB transform()

--- src/aabb.js
+++ src/aabb.js
@@ -9,6 +9,99 @@ var AABB = function ()
 AABB.extend(
 {
 	
+	fromVertices: function (vertices, stride)
+	{
+		if(stride == undefined)
+		{
+			stride = 3;
+		}
+
+		var min = vec3.fromValues(Number.MAX_VALUE, Number.MAX_VALUE, Number.MAX_VALUE);
+		var max = vec3.fromValues(Number.MIN_VALUE, Number.MIN_VALUE, Number.MIN_VALUE);
+
+		for(var i = 0; i < vertices.length / stride; i++)
+		{
+			var position = vec3.fromValues
+			(
+				vertices[i * stride + 0],
+				vertices[i * stride + 1],
+				vertices[i * stride + 2]
+			);
+
+			if(position[0] < min[0])
+			{
+				min[0] = position[0];
+			}
+
+			if(position[0] > max[0])
+			{
+				max[0] = position[0];
+			}
+
+			if(position[1] < min[1])
+			{
+				min[1] = position[1];
+			}
+
+			if(position[1] > max[1])
+			{
+				max[1] = position[1];
+			}
+
+			if(position[2] < min[2])
+			{
+				min[2] = position[2];
+			}
+
+			if(position[2] > max[2])
+			{
+				max[2] = position[2];
+			}
+		}
+
+		var diff = vec3.create();
+		vec3.subtract(diff, max, min);
+		vec3.scale(diff, diff, 0.5);
+		var center = vec3.create();
+		vec3.add(center, min, diff);
+		var extents = diff;
+
+		var aabb = new AABB();
+		aabb.center = center;
+		aabb.extents = extents;
+		return aabb;
+	},
+
+	transform: function (matrix)
+	{
+		var vertices = [];
+
+		var pushVertex = function (v0, v1, v2)
+		{
+			vertices.push(v0);
+			vertices.push(v1);
+			vertices.push(v2);
+		};
+
+		var topLeft = vec3.create();
+		vec3.subtract(topLeft, this.center, this.extents);
+
+		var bottomRight = vec3.create();
+		vec3.add(bottomRight, this.center, this.extents);
+
+		pushVertex(topLeft[0], topLeft[1], topLeft[2]);
+		pushVertex(topLeft[0], bottomRight[1], topLeft[2]);
+		pushVertex(bottomRight[0], topLeft[1], topLeft[2]);
+		pushVertex(bottomRight[0], bottomRight[1], topLeft[2]);
+
+		pushVertex(topLeft[0], topLeft[1], bottomRight[2]);
+		pushVertex(topLeft[0], bottomRight[1], bottomRight[2]);
+		pushVertex(bottomRight[0], topLeft[1], bottomRight[2]);
+		pushVertex(bottomRight[0], bottomRight[1], bottomRight[2]);
+
+		return AABB.fromVertices(vertices, 3);
+	},
+
 });
 
 AABB.prototype.extend(
