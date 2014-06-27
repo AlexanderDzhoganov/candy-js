f89dd5eb289f573c66969c5b0d1562bdede61f52
AlexanderMladenov <alexander.r.mladenov@gmail.com>
2014-06-27 20:47:30 +0300

    frustrum culling, implented planes, normalizatin f planes ...

--- src/frustum.js
+++ src/frustum.js
@@ -1,12 +1,26 @@
 include([ "plane" ]);
 
-var Frustum = function (projectionMatrix)
+var Frustum = function (m)
 {
+	this.left   = new Plane(m[3] + m[0], m[7] + m[4], m[11] + m[8], m[15] + m[12]);
+	this.right  = new Plane(m[3] - m[0], m[7] - m[4], m[11] - m[8], m[15] - m[12]);
+	this.bottom = new Plane(m[3] + m[1], m[7] + m[5], m[11] + m[9], m[15] + m[13]);
+	this.top    = new Plane(m[3] - m[1], m[7] - m[5], m[11] - m[9], m[15] - m[13]);
+	this.near   = new Plane(m[3] + m[2], m[7] + m[6], m[11] + m[10], m[15] + m[14]);
+	this.far    = new Plane(m[3] - m[2], m[7] - m[6], m[11] - m[10], m[15] - m[14]);
 
+	this.planes = [ this.left, this.right, this.bottom, this.top, this.near, this.far ];
 };
 
 Frustum.extend({
 	
+	INTERSECT_RESULT:
+	{
+		INSIDE = 0,
+		OUTSIDE = 1,
+		INTERSECT = 2,
+	},
+
 });
 
 Frustum.prototype.extend(
@@ -15,6 +29,61 @@ Frustum.prototype.extend(
 	transform: function (modelMatrix)
 	{
 
+	},
+
+	intersectAABB: function (aabb)
+	{
+		var bottomLeft = vec3.create();
+		vec3.subtract(bottomLeft, aabb.center, aabb.extents);
+
+		var topRight = vec3.create();
+		vec3.add(topRight, aabb.center, aabb.extents);
+
+		var vertices =
+		[
+			vec3.fromValues(bottomLeft[0], bottomLeft[1], bottomLeft[2]),
+			vec3.fromValues(bottomLeft[0], topRight[1],   bottomLeft[2]),
+			vec3.fromValues(topRight[0],   topRight[1],   bottomLeft[2]),
+			vec3.fromValues(topRight[0],   bottomLeft[1], bottomLeft[2]),
+			vec3.fromValues(bottomLeft[0], bottomLeft[1], topRight[2]),
+			vec3.fromValues(bottomLeft[0], topRight[1],   topRight[2]),
+			vec3.fromValues(topRight[0],   topRight[1],   topRight[2]),
+			vec3.fromValues(topRight[0],   bottomLeft[1], topRight[2]),
+		];
+
+		var outPoints = 0;
+		var inPoints = 0;
+		var result = Frustum.INTERSECT_RESULT.INSIDE;
+
+		for(var i = 0; i < 6; i++)
+		{
+			var plane = this.planes[i];
+
+			for(var q = 0; q < 8 && (inPoints == 0 || outPoints == 0); q++)
+			{
+				var vertex = vertices[q];
+				if(plane.distanceTo(vertex) < 0)
+				{
+					outPoints++;
+				}
+				else
+				{
+					inPoints++;
+				}
+			}
+
+			if(inPoints == 0)
+			{
+				return Frustum.INTERSECT_RESULT.OUTSIDE;
+			}
+
+			if(outPoints > 0)
+			{
+				result = Frustum.INTERSECT_RESULT.INTERSECT;
+			}
+		}
+
+		return result;
 	},
 
 });
--- src/plane.js
+++ src/plane.js
@@ -1,14 +1,37 @@
-var Plane = function ()
+var Plane = function (a, b, c, d)
 {
-	this.abcd = new Float32Array(4);
+	this.abcd = new Float32Array([a, b, c, d]);
+	this.normalize();
 };
 
 Plane.extend(
 {
 
+	halfspace: { NEGATIVE = -1, ON_PLANE = 0, POSITIVE = 1, },
+
 });
 
 Plane.prototype.extend(
 {
+
+	pointTest: function (point)
+	{
+		var d = this.distanceTo(point);
+		return d < 0.0 ? Plane.NEGATIVE : (d > 0.0 ? Plane.POSITIVE : Plane.ON_PLANE); 
+	},
+
+	distanceTo: function (point)
+	{
+		return this.abcd[0] * point[0] + 
+			   this.abcd[1] * point[1] + 
+			   this.abcd[2] * point[2] +
+			   	          this.abcd[3];
+	},
+
+	normalize: function ()
+	{
+		var magnitude = vec3.length(this.abcd);
+		vec4.scale(this.abcd, this.abcd, 1.0 / magnitude);
+	},
 
 });
